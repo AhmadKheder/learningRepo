0bd62a11514751053e33675644a442bf8fda099c
AhmadKheder <ahmadkhederabdulatef@gmail.com>
2020-04-30 15:36:58 +0300

    first js lab06

---
+++ js/content.js
@@ -0,0 +1,30 @@
+'use strict'
+
+//var nam = document.getElementById("fname");
+
+//var Age = document.getElementById("age");
+
+var Medicine = prompt("Food supplements you want","name it plz");
+
+var agree = prompt("How old Are you?");
+if(agree > 18){
+
+
+}
+if(agree >= 18 && !null){
+    confirm("Hey " +nam+"\nPress a button");
+    alert("lest Ask a Prof. Doctor")
+    window.open("https://www.doctorspring.com/ask-a-doctor-online");
+
+}else{
+    confirm("I'm Sorry, you are UNDERAGE hahahaha ^_*");
+    alert("get a new Toothbrush")
+
+}
+
+
+ 
+
+
+    
+
